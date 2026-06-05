b08b06c13968464f712841e09021327dd5f93839
User <user@Users-MacBook-Air.local>
2026-06-05 16:35:31 +0700

    fix: build config for production

---
+++ backend/src/@types/fastify.d.ts
@@ -0,0 +1,15 @@
+import 'fastify'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>
+  }
+  interface FastifyRequest {
+    user: {
+      userId: string
+      role: string
+      phone: string
+    }
+  }
+}
